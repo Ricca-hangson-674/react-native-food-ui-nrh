648279be1f7658128e8ef99e6c159b9d14172712
nrh <n.riccahang674@gmail.com>
2022-10-28 14:48:07 +0300

    initial 28/10/2022 14:48

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = function(api) {
+// eslint-disable-next-line func-names
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
+          alias: {
+            src: './src',
+          },
+        },
+      ],
+    ],
   };
 };
