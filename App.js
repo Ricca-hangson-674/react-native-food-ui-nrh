62d6f23ed159c4cab3d7421252ca17216838a69d
nrh <n.riccahang674@gmail.com>
2022-10-28 18:04:17 +0300

    update 28/10/2022 18:04

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {
+  useFonts,
+  Montserrat_400Regular as Montserrat400Regular,
+  Montserrat_500Medium as Montserrat500Medium,
+  Montserrat_600SemiBold as Montserrat600SemiBold,
+  Montserrat_700Bold as Montserrat700Bold,
+} from '@expo-google-fonts/montserrat';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.js to start working on your app!</Text>
-    {/* eslint-disable-next-line react/style-prop-object */}
-    <StatusBar style="auto" />
-  </View>
-);
+import Home from 'src/components/Home';
+
+const Stack = createStackNavigator();
+
+const screenOptionStyle = {
+  headerShown: false,
+};
+
+const App = () => {
+  useFonts({
+    Montserrat400Regular,
+    Montserrat500Medium,
+    Montserrat600SemiBold,
+    Montserrat700Bold,
+  });
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOptionStyle}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/data/popularData.js
@@ -0,0 +1,29 @@
+const pizza1 = require('src/assets/images/pizza1.jpg');
+const pizza2 = require('src/assets/images/pizza2.jpg');
+const pizza3 = require('src/assets/images/pizza3.jpg');
+
+const popularData = [
+  {
+    id: 1,
+    image: pizza1,
+    title: 'Primavera Pizza',
+    weight: '540 gr',
+    rating: '5.0',
+  },
+  {
+    id: 2,
+    image: pizza2,
+    title: 'Vegetarian Pizza',
+    weight: '450 gr',
+    rating: '4.0',
+  },
+  {
+    id: 3,
+    image: pizza3,
+    title: 'Pepperoni Pizza',
+    weight: '700 gr',
+    rating: '3.0',
+  },
+];
+
+export default popularData;
